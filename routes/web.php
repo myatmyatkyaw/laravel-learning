66e580257c59e3b18f1057fd5c67ee31113c9208
myatmyatkyaw <myatmyatkyaw@gicjp.com>
2024-01-03 13:32:01 +0630

    laravel route learning

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\resourcetest;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('hello', function () {
+    return view('test');
+});
+Route::resource('test',resourcetest::class);
+
